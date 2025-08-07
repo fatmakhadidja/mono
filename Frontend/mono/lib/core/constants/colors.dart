66dbb1113a23963d974bd5b840b3ba4c79d78dc4
Djerfi Fatma <khadidjadrf@gmail.com>
2025-08-07 13:14:18 +0100

    structured project and added colors and routes file

---
+++ Frontend/mono/lib/core/constants/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary colors
+  static const Color primary = Color(0xFF1B5C58); // Green
+
+  // Neutral colors
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color lightGrey = Color(0xFFAAAAAA); // Light grey
+  static const Color lightLightGrey = Color(0xFFFBFBFB);
+  static const Color darkGrey = Color(0xFF666666); // Darker grey
+
+  // Background
+  static const Color background = Color(0xFFFFFFFF); // Light background
+
+  // Alerts
+  static const Color error = Color(0xFFF95B51); // Red
+  static const Color success = Color(0xFF25A969); // Green
+  static const Color warning = Color(0xFFFBC02D); // Yellow
+}
---
+++ Frontend/mono/lib/routes/routes.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+// import '../ui/screens/home/home_screen.dart';
+// import '../ui/screens/auth/login_screen.dart';
+// import '../ui/screens/auth/register_screen.dart';
+// import '../ui/screens/transactions/transaction_list_screen.dart';
+
+// class AppRoutes {
+//   static const String home = '/home';
+//   static const String login = '/login';
+//   static const String register = '/register';
+//   static const String transactions = '/transactions';
+
+//   static Route<dynamic> generateRoute(RouteSettings settings) {
+//     switch (settings.name) {
+//       case home:
+//         return MaterialPageRoute(builder: (_) => HomeScreen());
+//       case login:
+//         return MaterialPageRoute(builder: (_) => LoginScreen());
+//       case register:
+//         return MaterialPageRoute(builder: (_) => RegisterScreen());
+//       case transactions:
+//         return MaterialPageRoute(builder: (_) => TransactionListScreen());
+//       default:
+//         return MaterialPageRoute(
+//           builder: (_) => Scaffold(
+//             body: Center(child: Text('No route defined for ${settings.name}')),
+//           ),
+//         );
+//     }
+//   }
+// }
