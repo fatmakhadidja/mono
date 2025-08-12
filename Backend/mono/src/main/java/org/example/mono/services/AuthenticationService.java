b336e52e596802ac20d53da564a1786c24531cf0
Djerfi Fatma <khadidjadrf@gmail.com>
2025-08-12 19:52:36 +0100

    authetntication and login done in backend

---
+++ Backend/mono/src/main/java/org/example/mono/services/AuthenticationService.java
@@ -0,0 +1,60 @@
+package org.example.mono.services;
+
+import lombok.RequiredArgsConstructor;
+import org.example.mono.auth.AuthenticationRequest;
+import org.example.mono.auth.AuthenticationResponse;
+import org.example.mono.auth.RegisterRequest;
+import org.example.mono.models.User;
+import org.example.mono.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    @Autowired
+ private UserRepo userRepo;
+
+ private final PasswordEncoder passwordEncoder;
+
+ private final JwtService jwtService;
+
+ private final AuthenticationManager authenticationManager;
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder().fullName(request.getFullName()).
+                email(request.getEmail()).
+                role(User.Role.USER).
+                password(passwordEncoder.encode(request.getPassword())).
+                build();
+        userRepo.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepo.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+
+//        var refreshToken = jwtService.generateRefreshToken(user);
+//        revokeAllUserTokens(user);
+//        saveUserToken(user, jwtToken);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
